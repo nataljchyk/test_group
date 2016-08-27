b13bee28c4a8273553e88990f7dba921f28f1453
nataljchyk <nataliia.bas@gmail.com>
2016-08-27 10:49:06 +0300

    Adding new file to pangram.py

--- pangram.py
+++ pangram.py
@@ -20,3 +20,4 @@ print(check('abcdefghijklmnopqrstuvwxyz') == True)
 print(check('ABCDEFGHIJKLMNOPQRSTUVWXYZ') == True)
 print(check('Quick brown fox jumps over the lazy dog') == True)
 print(check('dce') == False)
+# test file
